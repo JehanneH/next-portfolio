fe795f0d28cac31ae973795480ecc15ba39ff6a5
Jehanne Hodge <samarahjhodge@gmail.com>
2022-01-25 14:28:52 -0500

    add image and style project layout

--- components/Project_Preview.tsx
+++ components/Project_Preview.tsx
@@ -1,12 +1,19 @@
+import Image from 'next/image'
+
 const ProjectPreview = () => {
 
   const title: string = "Project 1";
   const desc: string = "Description of project here";
 
   return (
-    <div>
-      <a href={"/"} className="font-bold underline hover:text-indigo-600">{title}</a>
-      <div>{desc}</div>
+    <div className="flex gap-4">
+      <div className="relative w-24 h-16">
+        <Image src={"/code.jpg"} layout="fill"/>
+      </div>
+      <div className="my-auto">
+        <a href={"/"} className="font-bold underline hover:text-indigo-600">{title}</a>
+        <div>{desc}</div>
+      </div>
     </div>
   );
 }
